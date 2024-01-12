4398e27629d274fca1659a50ec7338702f70753f
Ali Savcı <asavci@bddk.org.tr>
2024-01-12 18:04:41 +0300

    Update 01_distributions.R

--- 01_distributions.R
+++ 01_distributions.R
@@ -1,9 +1,14 @@
 
 
+# create a new plotting window and set the plotting area into a 2*2 array
+par(mfrow=c(2,2))
+
 #normal dist
-norm_dist <- rnorm(1000, mean=10, sd=2)
+norm_dist <- rnorm(10000, mean=10, sd=2)
 hist(norm_dist)
+boxplot(norm_dist)
 
 #uniform distribution
-uniform_dist <- runif(1000)
+uniform_dist <- runif(10000)
 hist(uniform_dist)
+boxplot(uniform_dist)
